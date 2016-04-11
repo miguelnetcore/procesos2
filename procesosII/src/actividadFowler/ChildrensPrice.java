34f5199c5845a375db75033a059a8af9ee626fb7
Miguel Angel <miguelangel_24_7_92@hotmail.com>
2016-04-11 19:24:39 +0200

    childrens y releasePrice

--- procesosII/src/actividadFowler/ChildrensPrice.java
+++ procesosII/src/actividadFowler/ChildrensPrice.java
@@ -7,5 +7,10 @@ public class ChildrensPrice extends Price{
 		// TODO Auto-generated method stub
 		return Movie.CHILDRENS;
 	}
-
+	double getCharge(int daysRented){
+		 double result = 1.5;
+		 if (daysRented > 3)
+		 result += (daysRented - 3) * 1.5;
+		 return result;
+		 }
 }
--- procesosII/src/actividadFowler/NewReleasePrice.java
+++ procesosII/src/actividadFowler/NewReleasePrice.java
@@ -1,7 +1,11 @@
 package actividadFowler;
 
-public class NewReleasePrice extends Price{
-	 int getPriceCode() {
-		 return Movie.NEW_RELEASE;
+public class NewReleasePrice extends Price {
+	int getPriceCode() {
+		return Movie.NEW_RELEASE;
+	}
+
+	double getCharge(int daysRented) {
+		return daysRented * 3;
 	}
 }
