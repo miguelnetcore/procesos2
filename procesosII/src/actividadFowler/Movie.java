8b94339705b3c9627156a0c86d25f281e6cdc99a
Miguel Angel <miguelangel_24_7_92@hotmail.com>
2016-04-11 18:55:33 +0200

    cambiar descriptores de price

--- procesosII/src/actividadFowler/Movie.java
+++ procesosII/src/actividadFowler/Movie.java
@@ -5,19 +5,32 @@ public class Movie {
 	public static final int REGULAR = 0;
 	public static final int NEW_RELEASE = 1;
 	private String _title;
-	private int _priceCode;
+
+	public int getPriceCode() {
+		return _price.getPriceCode();
+	}
+
+	public void setPriceCode(int arg) {
+		switch (arg) {
+		case REGULAR:
+			_price = new RegularPrice();
+			break;
+		case CHILDRENS:
+			_price = new ChildrensPrice();
+			break;
+		case NEW_RELEASE:
+			_price = new NewReleasePrice();
+			break;
+		default:
+			throw new IllegalArgumentException("Incorrect Price Code");
+		}
+	}
+
+	private Price _price;
 
 	public Movie(String name, int priceCode) {
 		_title = name;
 		setPriceCode(priceCode);
-	}
-
-	public int getPriceCode() {
-		return _priceCode;
-	}
-
-	public void setPriceCode(int arg) {
-		_priceCode = arg;
 	}
 
 	public String getTitle() {
@@ -50,6 +63,5 @@ public class Movie {
 		else
 			return 1;
 	}
-
 
 }
